4a515109fa74880a12409efcb0dfa47ac1190796
LiSTlst <2510737554@qq.com>
2016-05-01 00:34:40 +0800

    rebuild

--- imagesGallery/script/script.js
+++ imagesGallery/script/script.js
@@ -1,5 +1,28 @@
+function prepareGallery(){
+	if(!document.getElementById) return false;
+	if(!document.getElementsByTagName) return false;
+	if(!document.getElementById("imagegallery")) return false;
+	var gallery=document.getElementById("imagegallery");
+	var links=gallery.getElementsByTagName("a");
+	for(var i=0;i<links.length;i++){
+		links[i].onclick=function(){
+			return showPic(this)?false:true;
+		}
+	}
+}
+
 function showPic(whichpic){
+	if(!document.getElementById("placeholder")) return false;
 	var source = whichpic.getAttribute("href");
 	var placeholder = document.getElementById("placeholder");
-	placeholder.setattribute("src",source);
+	if(placeholder.nodeName!="IMG") return false;
+	placeholder.setAttribute("src",source);
+	if(document.getElementById("description")){
+		var text=whichpic.getAttribute("title");
+		var description=document.getElementById("description")
+		if(description.firstChild.nodeType=3){
+			description.firstChild.nodeValue=text;
+		}
+	}
+	return true;
 }

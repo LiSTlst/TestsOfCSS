396ccaae711c64b1426d2d454e4831e67a4df078
LiSTlst <2510737554@qq.com>
2016-05-03 13:49:33 +0800

    imagegallery

--- imagesGallery/script/script.js
+++ imagesGallery/script/script.js
@@ -1,3 +1,17 @@
+window.onload=function(){
+	var placeholder=document.createElement("img");
+	placeholder.setAttribute("id","placeholder");
+	placeholder.setAttribute("src","images/placeholder.jpg");
+	placeholder.setAttribute("alt","my imagegallery");
+	var description=document.createElement("p");
+	description.setAttribute("id","description");
+	var text=document.createTextNode("chose an image");
+	description.appendChild(text);
+	document.body.appendChild(placeholder);
+	document.body.appendChild(description);
+	prepareGallery();
+}
+
 function prepareGallery(){
 	if(!document.getElementById) return false;
 	if(!document.getElementsByTagName) return false;
@@ -18,8 +32,8 @@ function showPic(whichpic){
 	if(placeholder.nodeName!="IMG") return false;
 	placeholder.setAttribute("src",source);
 	if(document.getElementById("description")){
-		var text=whichpic.getAttribute("title");
-		var description=document.getElementById("description")
+		var text=whichpic.getAttribute("title")?whichpic.getAttribute("title"):"";
+		var description=document.getElementById("description");
 		if(description.firstChild.nodeType=3){
 			description.firstChild.nodeValue=text;
 		}
